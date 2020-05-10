6fec57eda84383b4c0225623429da3618d87ee20
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-05-10 19:52:33 -0400

    Fixed deck generation scripts

--- src/generate_cards.py
+++ src/generate_cards.py
@@ -1,10 +1,11 @@
 import json
-from card import Card
+from src.game.card import Card
+from src.game.deck import Deck
 
 json_path = 'definitions/cards.json'
 json_path_spec = 'definitions/card-specials.json'
 
-deck = []
+deck = Deck()
 all_cards = {}
 with open(json_path, 'r') as card_defs:
     data = json.load(card_defs)
@@ -14,22 +15,22 @@ with open(json_path, 'r') as card_defs:
               f"\n{data['cards']['colors'][card_color]['numbers']}"
               f"\n{data['cards']['colors'][card_color]['special']}")
         for card_number in data['cards']['colors'][card_color]['numbers']:
-            deck.append(Card('regular', card_color, card_number=card_number, card_hex=data['cards']['colors'][card_color]['hex']))
-        for card_number in data['cards']['colors'][card_color]['special']:
-            deck.append(Card('regular', card_color, card_number=card_number, card_hex=data['cards']['colors'][card_color]['hex']))
+            deck.add_to_top(Card(card_category='regular', card_color=card_color, card_number=card_number, card_hex=data['cards']['colors'][card_color]['hex']))
+        for card_type in data['cards']['colors'][card_color]['special']:
+            deck.add_to_top(Card(card_category=card_type, card_color=card_color, card_number=-1, card_hex=data['cards']['colors'][card_color]['hex']))
     # Retrieve uncolored cards
     for unique in data['cards']['unique']:
         for i in range(int(data['cards']['unique'][unique]['amount'])):
             print(f"{unique} - "
                   f"\n{data['cards']['unique'][unique]}")
-            deck.append(Card(unique, 'none', card_number=-1, card_hex=data['cards']['unique'][unique]['hex']))
+            deck.add_to_top(Card(unique, 'none', card_number=-1, card_hex=data['cards']['unique'][unique]['hex']))
 
 all_cards['all_cards'] = []
-for x in deck:
+for x in list(deck.deck):
     item = x.get_json()
     all_cards['all_cards'].append(item)
 with open('test_files/test-all-cards.json', 'w') as out_file:
     json.dump(all_cards, out_file)
 
-print('\n'.join(x.get_json_dump() for x in deck))
-print(f'Deck Size: {len(deck)}')
+print('\n'.join(x.get_json_dump() for x in list(deck.deck)))
+print(f'Deck Size: {len(deck.deck)}')
