1a0b49d367ddbe86fa5d734ccfaca78d9932489c
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-19 21:44:22 -0400

    The player hand is now treated as a deck

--- src/game/player.py
+++ src/game/player.py
@@ -1,12 +1,13 @@
 from typing import List, Optional
 from src.game.card import Card
+from src.game.deck import Deck
 
 
 class Player:
-    def __init__(self, socket, name, hand=Optional[List[Card]]):
+    def __init__(self, socket, name, hand: Optional[List[Card]] = None):
         self._socket = socket
         self._name = name
-        self._hand = hand
+        self._hand = hand if hand is not None else Deck()
         self.name_set_flag = False
 
     @property
@@ -14,8 +15,17 @@ class Player:
         return self._hand
 
     @hand.setter
-    def hand(self, value: Optional[List[Card]]):
-        self._hand = value
+    def hand(self, deck: Optional[List[Card]]):
+        self._hand = deck
+
+    def add_to_hand(self, card: Card):
+        self._hand.add_to_top(card)
+
+    def remove_from_hand(self, card: Card):
+        self._hand.remove_card(card)
+
+    def hand_size(self):
+        return self._hand.size
 
     @property
     def name(self):
