268c6a2b696b594843888f64a92f12c44e3d63a9
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-20 21:55:33 -0400

    Added discard pile, first card of the game, and first player choice

--- src/game/logic/game.py
+++ src/game/logic/game.py
@@ -2,6 +2,7 @@ from src.game.deck import Deck
 from src.server import server_data
 from src.server.server_strings import *
 import time
+import random
 
 end_flag = False
 
@@ -15,6 +16,7 @@ def clear_game(server):
 
 def game_loop(server):
     game_deck = Deck()
+    discard_pile = Deck()
     game_deck.generate_default_deck(num_of_players=len(server.clients), card_definitions_file='../definitions/cards.json')
     print(f"Deck Size: {game_deck.size}")
     server.send_message(server_data.Data(content_type=SERV_MESSAGE, content_data=f'THE GAME HAS STARTED!', client=server.name))
@@ -24,6 +26,16 @@ def game_loop(server):
             server.clients[client].add_to_hand(card)
             server.send_message(server_data.Data(content_type=SERV_MESSAGE, content_data=f'Given {server.clients[client].name}: {card.get_json()}', client=server.name), sock=server.clients[client].socket)
             print(f'Cards left in deck: {game_deck.size}')
+
+    # Draw first card
+    discard_pile.add_to_top(game_deck.draw())
+    print(f"Chosen Card - {discard_pile.get_top_card()}")
+    server.send_message(server_data.Data(content_type=SERV_BROADCAST, content_data=f'Discard Pile: Top Card - {discard_pile.get_top_card()}'))
+
+    # Get first player
+    current_player = random.choice(list(server.clients.values()))
+    print(f"First Player - {current_player.name}")
+    server.send_message(server_data.Data(content_type=SERV_BROADCAST, content_data=f'The first player is {current_player.name}'))
 
     while not end_flag:
         time.sleep(1)
