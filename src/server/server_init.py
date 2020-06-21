aa6294df3d3cdb5ce0825354d6e8ef77895d08e7
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-21 01:46:43 -0400

    Added card playing and drawing functionality in game logic

--- src/server/server_init.py
+++ src/server/server_init.py
@@ -66,10 +66,12 @@ def on_get_clients(*params):
 
 def on_draw_card(*params):
     socket = params[0]
-    from src.game.deck import Deck
-    deck = Deck(infinite_deck=True)
-    card = deck.draw()
-    server.send_message(server_data.Data(content_type=SERV_MESSAGE, content_data=f'{str(card)}'), sock=socket)
+    if game_started:
+        if game.current_player.socket == socket:
+            card = game.game_deck.draw()
+            game.current_player.add_to_hand(card)
+            server.send_message(server_data.Data(content_type=SERV_MESSAGE, content_data=f'You drew the card ({card.color}:{card.value})'), sock=game.current_player.socket)
+            game.end_turn(server)
 
 
 def on_view_cards(*params):
@@ -80,8 +82,8 @@ def on_view_cards(*params):
             server.send_message(server_data.Data(content_type=SERV_MESSAGE, content_data=f'{client.name} has no cards on hand.'), sock=socket)
             # server.outputs.append(socket)
             return
-        for card in client.hand.deck:
-            server.send_message(server_data.Data(content_type=SERV_MESSAGE, content_data=f'{str(card)}'), sock=socket)
+        for i, card in enumerate(client.hand.deck):
+            server.send_message(server_data.Data(content_type=SERV_MESSAGE, content_data=f'[{i}]({card.color}:{card.value})'), sock=socket)
 
 
 def on_game_start(*params):
@@ -104,6 +106,31 @@ def on_game_stop(*params):
         game.clear_game(server=server)
 
 
+def on_player_play_card(*params):
+    socket = params[0]
+    try:
+        card_index = int(params[1])
+    except TypeError:
+        return
+    global game_started
+    if game_started:
+        if game.current_player.socket == socket:
+            my_card = None
+            for i, card in enumerate(game.current_player.hand):
+                # print(f"ITERATING HAND - Card({card.color}:{card.value})")
+                if i == card_index:
+                    my_card = card
+                    # print(f"FOUND CARD - Card({card.color}:{card.value})")
+            if my_card is None:
+                return
+            if my_card.color == game.discard_pile.get_top_card().color or my_card.value == game.discard_pile.get_top_card().value:
+                # print(f"CARD PLAYED - Card({my_card.color}:{my_card.value})")
+                removed_card = game.current_player.remove_from_hand(my_card)
+                game.discard_pile.add_to_top(removed_card)
+                server.send_message(server_data.Data(content_type=SERV_BROADCAST, content_data=f'{game.current_player.name} played ({my_card.color}:{my_card.value})'))
+                game.end_turn(server)
+
+
 game_started = False
 server = Server(name='UnoServer')
 server.callbacks.register_callback('on_client_connect', on_connect)
@@ -115,10 +142,12 @@ server.callbacks.register_callback('on_get_clients', on_get_clients)
 server.callbacks.register_callback('on_game_start', on_game_start)
 server.callbacks.register_callback('on_game_stop', on_game_stop)
 server.callbacks.register_callback('on_view_cards', on_view_cards)
+server.callbacks.register_callback('on_play_card', on_player_play_card)
 server.commands.register_command('!say', 'on_client_message')
 server.commands.register_command('!draw', 'on_draw_card')
 server.commands.register_command('!cards', 'on_view_cards')
 server.commands.register_command('!clients', 'on_get_clients')
 server.commands.register_command('!start', 'on_game_start')
 server.commands.register_command('!stop', 'on_game_stop')
+server.commands.register_command('!play', 'on_play_card')
 server.start_server()
